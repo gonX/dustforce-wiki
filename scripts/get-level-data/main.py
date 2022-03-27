e93e1cfd92fa49ec168fe41b12ba693d18e82cbc
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2022-03-27 08:29:58 +0200

    Dustmaker 1.1 support

--- scripts/get-level-data/main.py
+++ scripts/get-level-data/main.py
@@ -2,6 +2,8 @@
 # needs 'pip install --user dustmaker'
 
 from dustmaker import *
+from dustmaker.tile import TileSpriteSet
+from dustmaker.entity import *
 import json
 import sys
 import os
@@ -12,35 +14,37 @@ import wiki_output_format
 
 records_url = "https://dustkid.com/json/records"
 records = {}
+jsonobj = {}
 print(f"Pulling record data from {records_url}")
 jsonobj = requests.get(records_url).json()
-for x in jsonobj["Scores"]:
-    mapoutput = {}
-    for y in jsonobj:
-        mapobj = jsonobj[y][x]
-        mapoutput["best_" + y[:-1].lower() + "_time_ms"] = mapobj["time"]
-        mapoutput["best_" + y[:-1].lower() + "_timestamp_epoch"] = mapobj["timestamp"]
-        mapoutput["best_" + y[:-1].lower() + "_username"] = mapobj["username"]
-        mapoutput["best_" + y[:-1].lower() + "_character"] = wiki_output_format.Character(mapobj["character"])
-        mapoutput["best_" + y[:-1].lower() + "_replay_id"] = mapobj["replay_id"]
+if 'Scores' in jsonobj:
+    for x in jsonobj["Scores"]:
+        mapoutput = {}
+        for y in jsonobj:
+            mapobj = jsonobj[y][x]
+            mapoutput["best_" + y[:-1].lower() + "_time_ms"] = mapobj["time"]
+            mapoutput["best_" + y[:-1].lower() + "_timestamp_epoch"] = mapobj["timestamp"]
+            mapoutput["best_" + y[:-1].lower() + "_username"] = mapobj["username"]
+            mapoutput["best_" + y[:-1].lower() + "_character"] = wiki_output_format.Character(mapobj["character"])
+            mapoutput["best_" + y[:-1].lower() + "_replay_id"] = mapobj["replay_id"]
 
-    records[x] = mapoutput
+        records[x] = mapoutput
 
 def generateMapData(mapfile):
     with open(mapfile, "rb") as f:
-        map = read_map(f.read())
-        print("Reading map '{}' ({})".format(map.name(), os.path.basename(mapfile)))
+        map = read_level(f.read())
+        print("Reading map '{}' ({})".format(map.name.decode("utf-8"), os.path.basename(mapfile)))
 
         output = copy.deepcopy(wiki_output_format.output_format)
-        output["name"] = map.name()
+        output["name"] = map.name.decode("utf-8")
         output["srcfile"] = os.path.basename(mapfile)
-        output["key_get_type"] = wiki_output_format.Keytype(map.vars["key_get_type"].value)
+        output["key_get_type"] = wiki_output_format.Keytype(map.variables["key_get_type"].value)
         output["props"] = len(map.props)
         # keep in mind Beginner Tutorial does not have the flag, but the game is hardcoded to use it
-        if "Beginner Tutorial" in map.name():
+        if "Beginner Tutorial" in map.name.decode("utf-8"):
             output["is_virtual"] = True
         else:
-            output["is_virtual"] = map.virtual_character()
+            output["is_virtual"] = map.virtual_character
 
         # tile layers
         for ((layer, x, y), tile) in map.tiles.items():
@@ -53,10 +57,10 @@ def generateMapData(mapfile):
             if tile.has_filth():
                 output["covered_tiles"] += 1
                 # check all faces of tile for "filth"
-                for orientation in TileSide:
-                    if tile.edge_filth_sprite(orientation)[0] == TileSpriteSet.none_0:
+                for orientation in tile.edge_data:
+                    if orientation.filth_sprite_set == TileSpriteSet.NONE_0:
                         continue
-                    if tile.edge_filth_sprite(orientation)[1]:
+                    if orientation.filth_spike:
                         output["spiked_surfaces"] += 1
                     else:
                         output["filthy_surfaces"] += 1
@@ -66,7 +70,7 @@ def generateMapData(mapfile):
         for (id, (x, y, entity)) in list(map.entities.items()):
             if isinstance(entity, Enemy):
                 output["enemies"] += 1
-                output["dust_from_enemies"] += entity.combo()
+                output["dust_from_enemies"] += entity.FILTH
                 if not entity.TYPE_IDENTIFIER in output:
                     output[entity.TYPE_IDENTIFIER] = 0
                     print("Enemy {} is not in wiki_output_format!".format(entity.TYPE_IDENTIFIER), file=sys.stderr)
@@ -96,7 +100,6 @@ if __name__ == '__main__':
     num_cores = multiprocessing.cpu_count()
 
     results = Parallel(n_jobs=num_cores)(delayed(generateMapData)(current_map) for current_map in [localpath + f for f in os.listdir(localpath) if os.path.isfile(os.path.join(localpath, f))])
-
     topdir = subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode('utf-8').rstrip()
     with open(os.path.join(topdir, "website", "_data", "stock-maps.json"), 'w+') as f:
         f.write(json.dumps(sorted(results,key=lambda x: x["srcfile"].lower()), indent=2))
